7d4026863265c3080263fb59a8048a110c79cf8c
Eugene <umputun@gmail.com>
2021-12-11 01:45:39 -0600

    more status test

--- app/status/status_test.go
+++ app/status/status_test.go
@@ -11,8 +11,13 @@ func TestService_Get(t *testing.T) {
 	svc := Service{Volumes: []Volume{{Name: "root", Path: "/"}}}
 	res, err := svc.Get()
 	require.NoError(t, err)
+	t.Logf("%+v", res)
 	assert.Equal(t, 1, len(res.Volumes))
 	assert.Equal(t, "root", res.Volumes["root"].Name)
 	assert.Equal(t, "/", res.Volumes["root"].Path)
 	assert.True(t, res.Volumes["root"].UsagePercent > 0)
+	assert.True(t, res.CPUPercent > 0)
+	assert.True(t, res.MemPercent > 0)
+	assert.True(t, res.Loads.One > 0)
+	assert.True(t, res.Uptime > 0)
 }
