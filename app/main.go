ae063cac3531b22448c54e4ed62d1121b585e2c4
Umputun <umputun@gmail.com>
2022-02-06 13:22:07 -0600

    add program provided allowing to check exit code

--- app/main.go
+++ app/main.go
@@ -68,9 +68,10 @@ func main() {
 	}
 
 	providers := external.Providers{
-		HTTP:   &external.HTTPProvider{Client: http.Client{Timeout: opts.TimeOut}},
-		Mongo:  &external.MongoProvider{TimeOut: opts.TimeOut},
-		Docker: &external.DockerProvider{TimeOut: opts.TimeOut},
+		HTTP:    &external.HTTPProvider{Client: http.Client{Timeout: opts.TimeOut}},
+		Mongo:   &external.MongoProvider{TimeOut: opts.TimeOut},
+		Docker:  &external.DockerProvider{TimeOut: opts.TimeOut},
+		Program: &external.ProgramProvider{TimeOut: opts.TimeOut, WithShell: true},
 	}
 
 	srv := server.Rest{
